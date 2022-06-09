8a4ec901cc301b3cb2b25ee8604cd8de8bdf4e4d
giovanni <giovanni.manzini@gmail.com>
2022-06-09 18:42:28 +0200

    complex values ok

--- mat2csrv.cpp
+++ mat2csrv.cpp
@@ -10,13 +10,19 @@
 #include <complex>
 #include <array>
 
+// bit representing types of input/output values 
+#define INT32_OUTPUT 1
+#define FLOAT_OUTPUT 2
+#define COMPLEX_INPUT 4
+
+
 static void usage_and_exit(char *name)
 {
     fprintf(stderr,"Usage:\n\t  %s [options] matrix rows cols\n",name);
     fprintf(stderr,"\t\t-b num         number of row blocks, def. 1\n");
     fprintf(stderr,"\t\t-c             input are complex numbers\n");
-    fprintf(stderr,"\t\t-f             save entries as float\n");
-    fprintf(stderr,"\t\t-i             save entries as int32\n");
+    fprintf(stderr,"\t\t-f             save entries as floats\n");
+    fprintf(stderr,"\t\t-i             save entries as int32s\n");
     fprintf(stderr,"\t\t-v             verbose\n\n");
     exit(1);
 }
@@ -38,15 +44,44 @@ int bits (unsigned long n) {
   return b;
 }
 
-typedef struct {
-  double x;
-  double y;
-} dpair;
+
+// write a double x as a int32/float/double in binary format  
+void write_bin(double x, int out_type, FILE *f)
+{
+  int e;
+  
+  if(out_type & INT32_OUTPUT) {
+    int32_t y = (int32_t) lround(x);
+    e = fwrite(&y,sizeof(y),1,f);
+  }
+  else if(out_type & FLOAT_OUTPUT) {
+    float y = (float) x;
+    e = fwrite(&y,sizeof(y),1,f);
+  }
+  else // double
+    e = fwrite(&x,sizeof(x),1,f);
+    
+  if(e!=1) quit("Error writing to binary file");
+} 
+
+
+// provide hash function for a complex<double> 
+struct ComplexHasher
+{
+  size_t operator()(const std::complex<double>& k) const
+  {
+    using std::hash;
+
+    return ((hash<double>()(k.real())
+             ^ (hash<double>()(k.imag()) << 1)) >> 1);
+  }
+};
+
 
 int main (int argc, char **argv) { 
   extern char *optarg;
   extern int optind, opterr, optopt;
-  int verbose=0,fvalues=0,ivalues=0,cvalues=0;
+  int verbose=0,vtype=0;
   int c,rows,cols,nblocks=1;
   char fname[PATH_MAX];
   time_t start_wc = time(NULL);
@@ -60,11 +95,11 @@ int main (int argc, char **argv) {
       case 'v':
         verbose++; break;
       case 'f':
-        fvalues++; break;
+        vtype |= FLOAT_OUTPUT; break;
       case 'i':
-        ivalues++; break;
+        vtype |= INT32_OUTPUT; break;
       case 'c':
-        cvalues++; break;
+         vtype |= COMPLEX_INPUT; break;
       case 'b':
         nblocks=atoi(optarg); break;
       case '?':
@@ -83,7 +118,7 @@ int main (int argc, char **argv) {
     fprintf(stderr,"Error! Option -b must be at least one\n");
     usage_and_exit(argv[0]);
   }  
-  if(ivalues>0 && fvalues>0) {
+  if( (vtype&INT32_OUTPUT) && (vtype&FLOAT_OUTPUT) ) {
     fprintf(stderr,"Error! Options -f and -i are mutually exclusive\n");
     usage_and_exit(argv[0]);
   }  
@@ -114,17 +149,16 @@ int main (int argc, char **argv) {
   // init counters
   int  r = 0;   // number of read rows
   int  wr = 0;  // number of written rows
-  unsigned long   nonz = 0;        // total number of nonzeros  
-  unsigned long  dnonz = 0;        // distinct nonzeros  
-  unsigned long  maxcode = 0;      // largest code in a .vc file
+  unsigned long    nonz = 0;      // total number of nonzeros  
+  unsigned long   dnonz = 0;      // distinct nonzeros  
+  unsigned long maxcode = 0;      // largest code in a .vc file
   // variables for real input values
   std::unordered_map<double,unsigned long> values; // dictionary of distinct nonzero
   double v;   // values read from file
   // same for complex values
-  std::map<std::complex<double>,unsigned long, std::less<std::complex<double>>> covalues; // dictionary of distinct nonzero
+  std::unordered_map<std::complex<double>,unsigned long, ComplexHasher> covalues; // dictionary of distinct nonzero
   std::complex<double> cov;
   double re,im;
-  float x[2];
   
   // main loop reading csv file 
   size_t n=0;
@@ -156,42 +190,39 @@ int main (int argc, char **argv) {
           quit("Check input file");
         }
         char *tmp;
-        // ---- read a value from file fouble or complex
-        if(!cvalues) { // plain real value
+        // ---- read a value from file either a double or a complex<double>
+        if(vtype&COMPLEX_INPUT) { // complex value
+          int e = sscanf(s," (%lf%lfj)",&re,&im);
+          if(e!=2) {
+            fprintf(stderr,"Invalid or missing complex entry in row %d column %d (one based)\n",r,c+1);
+            quit("Check input file");
+          }
+          v= (re==0 and im ==0) ? 0 : 1; // v==0 iff cov==0
+          cov.real(re); cov.imag(im);    // init cov          
+        }
+        else { // plain real value
           v = strtod(s,&tmp);
           if(tmp==s) {
             fprintf(stderr,"Missing value in row %d column %d (one based)\n",r,c+1);
             quit("Check input file");
           }
         }
-        else { // complex value
-          int e = sscanf(s,"(%lf%lfj)",&re,&im);
-          if(e!=2) {
-            fprintf(stderr,"Invalid complex entry in row %d column %d (one based)\n",r,c+1);
-            quit("Check input file");
-          }
-          v= (re==0 and im ==0) ? 0 : 1; // v==0 iff cov==0
-          cov.real(re); cov.imag(im);    // init cov
-        }
         // ----- process element if nonzero 
         if(v!=0) { // process non zero value
           unsigned long id, code;
           nonz += 1;
           // get id of entry and possibly write new values to .val file
-          if(!cvalues) { // real entry
+          if(!(vtype&COMPLEX_INPUT)) { // real entry
             if(values.count(v)==0) {
               id = values[v] = dnonz++;
-              if(fwrite(&v,sizeof(v),1,fval)!=1)
-                quit("Error writing to .val file");
+              write_bin(v,vtype,fval);
             } else id = values[v];
           } else { // complex entry 
             if(covalues.count(cov)==0) {
-              id = values[v] = dnonz++;
-              x[0] = (float) cov.real(); 
-              x[1] = (float) cov.imag();
-              if(fwrite(x,2*sizeof(*x),2,fval)!=2)
-                quit("Error writing to .val file");
-            } else id = covalues[v];
+              id = covalues[cov] = dnonz++;
+              write_bin(cov.real(),vtype,fval);
+              write_bin(cov.imag(),vtype,fval);
+            } else id = covalues[cov];
           }
           // generate code and write it to .vc file 
           code = id*cols + c;
@@ -214,17 +245,19 @@ int main (int argc, char **argv) {
       if(fwrite(&wcode,sizeof(wcode),1,fvc)!=1) 
         quit("Error writing to .vc file");
       wr += 1;
-      // if a block is full stop and start the next 
+      // if a block is full, stop and start the next 
       if ((wr%block_size) == 0) break; // end while true
     }
     fclose(fvc);
   }
   fclose(fval);
+  if(vtype&COMPLEX_INPUT) assert(dnonz==covalues.size());
+  else assert(dnonz==values.size());
   if(wr!=rows)
     fprintf(stderr, "Warning! Written %d rows instead of %d\n", wr, rows);
   fprintf(stderr,"Elapsed time: %.0lf secs\n",(double) (time(NULL)-start_wc));  
   fprintf(stderr,"Number of nonzeros: %ld Nonzero ratio: %.4f\n", nonz, ((double) nonz/(wr*cols)));  
-  fprintf(stderr, "%zd distinct nonzeros values\n", values.size());
+  fprintf(stderr, "%zd distinct nonzeros values\n", dnonz);
   fprintf(stderr,"Largest codeword: %lu bits: %d\n", maxcode, bits(maxcode));
   fprintf(stderr,"==== Done\n");
   
